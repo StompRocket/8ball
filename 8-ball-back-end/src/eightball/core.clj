558e4393666c3e87a166bfdf177d64047be23007
swissChili <sasha@fastmail.com>
2018-09-07 18:37:22 -0700

    Flawless programming complete, does basically nothing

--- 8-ball-back-end/src/eightball/core.clj
+++ 8-ball-back-end/src/eightball/core.clj
@@ -1,7 +1,20 @@
 (ns eightball.core
   (:gen-class))
 
+(defn serve
+  "serves to the provided port and host"
+  [host port]
+  host)
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "finds meaning of query"
   [& args]
+  (cond
+    (= (count args) 1)
+    (do
+      (println (first args)))
+    (and (= (count args) 3)
+         (= (first args) "--serve"))
+    (do
+      (println "serving on" (second args) ":" (nth args 2)))); indexing from 0 :)
   (println "Hello, World!"))
